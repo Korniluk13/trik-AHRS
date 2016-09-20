bc4e5b01b5f0c027c96d845525d05b1c7b2727f3
Korniluk13 <korniluk13@mail.ru>
2016-09-20 21:34:45 +0300

    simple quaternion integrator

---
+++ quaternionIntegration/method1/main.cpp
@@ -0,0 +1,28 @@
+#include <QtCore/qglobal.h>
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+	#include <QtGui/QApplication>
+#else
+	#include <QtWidgets/QApplication>
+#endif
+
+#include <QtCore/QScopedPointer>
+
+#include <trikControl/brickInterface.h>
+#include <trikControl/brickFactory.h>
+
+#include "integrator.h"
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+
+	QScopedPointer<trikControl::BrickInterface> brick(trikControl::BrickFactory::create());
+
+	Integrator integrator(*brick);
+
+	QObject::connect(brick->gyroscope(), SIGNAL(newData(QVector<int>,const trikKernel::TimeVal)),
+					 &integrator, SLOT(integrate(QVector<int>,const trikKernel::TimeVal)));
+
+	return app.exec();
+}
